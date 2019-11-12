65ab3e2b7321bd9dfd612756def44717e51713f4
Johannes Thoma <johannes@johannesthoma.com>
2019-11-12 14:00:27 +0100

    Have SANBOOT_WINDRBD compilation time feature macro.

--- src/config/defaults/pcbios.h
+++ src/config/defaults/pcbios.h
@@ -43,6 +43,12 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
 #define	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
 #define SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#define SANBOOT_WINDRBD		/* WinDRBD (via http and iBFT) */
+
+#ifdef SANBOOT_WINDRBD
+#define	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
+#define SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#endif
 
 #define	USB_HCD_XHCI		/* xHCI USB host controller */
 #define	USB_HCD_EHCI		/* EHCI USB host controller */
